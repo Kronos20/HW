d75bceb3431c35fdb09115944c2d9b360f8cad67
Kronos20 <vorobev.2001@gmail.com>
2022-01-20 20:38:37 +0500

    Modified

--- Homew/Homew/ViewController.swift
+++ Homew/Homew/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func didTapOnLogin(_ sender: Any) {
+        guard let loginText = loginTextField.text,
+        let passwordText = passwordTextField.text else{
+            return
+        }
+        if loginText == "hello" && passwordText == "12345"{
+            print("Successful login")
+        }
+    }
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        return checkForValidationData()
+    }
+    func checkForValidationData() -> Bool {
+        guard let loginText = loginTextField.text,
+              let passwordText = passwordTextField.text else{
+                  return false
+              }
+        let isValidData = loginText == "hello" && passwordText == "12345"
+        return isValidData
+    }
 }
 
